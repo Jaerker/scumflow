f9a1b471527cd065192190fa957f5764e1e65df4
Jaerker <36820009+Jaerker@users.noreply.github.com>
2024-03-06 14:21:28 +0100

    added exception handling

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ fetchPokemonDetails('https://pokeapi.co/api/v2/pokemon/1/');
 async function fetchPokemonDetails(url){
 
 
-    const data = await fetch(url).then(res => {return res.json()});
+
+    const data = await fetch(url).then(res => {return res.json()}).catch(e => e.json());
 
     return data;
 
